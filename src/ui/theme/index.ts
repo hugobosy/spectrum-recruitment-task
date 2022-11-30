b33dc311da8c2160099b2887dd8a32ba906e06eb
HugoBosy <hubert.kliszcz@gmail.com>
2022-11-30 22:26:55 +0100

    add radius variables

--- src/ui/theme/index.ts
+++ src/ui/theme/index.ts
@@ -1,9 +1,11 @@
 import { color } from "./color";
 import { font } from "./font";
 import { media } from "./media";
+import { radius } from "./radius";
 
 export const theme = {
   color,
   font,
   media,
+  radius,
 };
---
+++ src/ui/theme/radius.ts
@@ -0,0 +1,5 @@
+export const radius = {
+  normal: "20px",
+  medium: "200px 20px 100px 500px",
+  large: "500px 60px 300px 100px",
+};
