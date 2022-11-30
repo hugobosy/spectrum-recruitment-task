e62bcae037acc7a4f04a2333e3cf24ec146ca823
HugoBosy <hubert.kliszcz@gmail.com>
2022-11-30 21:18:40 +0100

    add colors variables theme

---
+++ src/ui/theme/color.ts
@@ -0,0 +1,18 @@
+export const color = {
+  dark: "#000",
+  light: "#fff",
+
+  gray: {
+    dark: "#362C2C",
+    light: "#D9D9D9",
+  },
+
+  success: {
+    light: "#19940E",
+    normal: "#35660E",
+    dark: "#40483A",
+  },
+
+  primary: '#071594',
+  danger: "#FF0000",
+};
